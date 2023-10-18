20cb3ba87cae28f4818735dcd84de92956352120
Сдобнов Владимир Александрович <s22380352@unn.ru>
2023-10-18 11:34:16 +0300

    Delete tab example.cpp

--- lib_example/example.cpp
+++ lib_example/example.cpp
@@ -4,6 +4,6 @@
 #include "../lib_example/example.h"
 
 float division(int a, int b) {
-	if (b == 0) throw std::invalid_argument("Input Error: can't divide by zero!");
+    if (b == 0) throw std::invalid_argument("Input Error: can't divide by zero!");
 	return static_cast<float>(a) / b;
 }
